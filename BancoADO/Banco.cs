0a9e45cb2f6a3ca41878d5a14d2640560401e33d
Infinite System <81973574+InfiniteSystem20@users.noreply.github.com>
2021-05-10 10:22:53 -0300

    Atualização da class
    Classe Banco

--- BancoADO/Banco.cs
+++ BancoADO/Banco.cs
@@ -1,12 +1,44 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Configuration;
+using System.Data;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace BancoADO
 {
-    class Banco
+    public class Banco : IDisposable
     {
+        private readonly MySqlConnection conexao;
+        public Banco()
+        {
+            conexao = new MySqlConnection(ConfigurationManager.ConnectionStrings["conexao"].ConnectionString);
+            conexao.Open();
+
+        }
+
+        public void ExecutaComando(string strQuery)
+        {
+            var vComando = new MySqlCommand
+            {
+                CommandText = strQuery,
+                CommandType = CommandType.Text,
+                Connection = conexao
+            };
+            vComando.ExecuteNonQuery();
+
+        }
+        public MySqlDataReader RetornaComando(string strQuery)
+        {
+            var vComando = new MySqlCommand(strQuery, conexao);
+            return vComando.ExecuteReader();
+        }
+
+
+        public void Dispose()
+        {
+            if (conexao.State == ConnectionState.Open)
+                conexao.Close();
+        }
     }
 }
