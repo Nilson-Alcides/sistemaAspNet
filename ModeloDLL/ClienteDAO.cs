ef4a5404a679236f49891bff8c11bdfefb7166ea
Infinite System <81973574+InfiniteSystem20@users.noreply.github.com>
2021-05-10 11:13:45 -0300

    Criação do projeto ModeloDLL
    Atualização das classes

---
+++ ModeloDLL/ClienteDAO.cs
@@ -0,0 +1,32 @@
+﻿using BancoADO;
+using Dominios;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModeloDLL
+{
+    public class ClienteDAO
+    {
+        private Banco db = new Banco();
+
+        public void Insert(Cliente cliente)
+        {
+            string strInsert = string.Format("insert into cliente(login_id_cli, senha_cli, nome_cli, cpf_cli, telefone_cli, " +
+                "endereco_cli, complemento_cli, bairro_cli, cidade_cli, uf_id_est, cep_cli) " +
+                    "values('{0}','{1}','{2}','{3}','{4}','{5}','{6}','{7}','{8}','{9}','{10}','{11}');",
+                    cliente.login_id_cli, cliente.senha_cli, cliente.nome_cli, cliente.telefone_cli, cliente.endereco_cli,
+                    cliente.complemento_cli, cliente.bairro_cli, cliente.cidade_cli, cliente.uf_id_est, cliente.cep_cli);
+
+            db.ExecutaComando(strInsert);
+        }
+        public MySqlDataReader Select()
+        {
+            string strString = "select * From cliente ";
+            return db.RetornaComando(strString);
+        }
+    }
+}
---
+++ ModeloDLL/UsuarioDAO.cs
@@ -0,0 +1,51 @@
+﻿using BancoADO;
+using Dominios;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModeloDLL
+{
+    public class UsuarioDAO
+    {
+        private Banco db = new Banco();
+
+        public void Insert(Usuario usuario)
+        {
+            //string strInsert = string.Format("insert into tbUSUARIO(NomeUsu, Cargo, DataNasc) " +
+            //                               "values('{0}','{1}',STR_TO_DATE( '{2}', '%d/%m/%Y' ));"
+            //, usuario.NomeUsu, usuario.Cargo, usuario.DataNasc);
+
+            string strInsert = string.Format("insert into tbUSUARIO(NomeUsu, Cargo, SenhaUsu, DataNasc) " +
+                    "values('{0}','{1}','{2}','{3}');", usuario.NomeUsu, usuario.Cargo, usuario.Senha, (usuario.DataNasc).ToString("yyyy-MM-dd"));
+
+            db.ExecutaComando(strInsert);
+        }
+
+        public void Update(Usuario usuario)
+        {
+            string strInsert = "update tbUsusrio set ";
+            strInsert += string.Format("NomeUsu = '{0}',", usuario.NomeUsu);
+            strInsert += string.Format("Cargo = '{0}',", usuario.Cargo);
+            strInsert += string.Format("SenhaUsu = '{0}',", usuario.Senha);
+            strInsert += string.Format("DataNasc = '{0}',", (usuario.DataNasc).ToString("yyyy-MM-dd"));
+            strInsert += string.Format("where IdUsu = '{0}',", usuario.IdUsu);
+            db.ExecutaComando(strInsert);
+        }
+
+        public void Delete(Usuario usuario)
+        {
+            string strInsert = "delete from tbUsusrio";
+            strInsert += string.Format("where IdUsu = '{0}',", usuario.IdUsu);
+            db.ExecutaComando(strInsert);
+        }
+
+        public MySqlDataReader Select()
+        {
+            string strString = "select * From tbUSUARIO ";
+            return db.RetornaComando(strString);
+        }
+
+    }
+}
