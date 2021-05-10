e1184495add8aca65a3275c7eea143d9dc12ecd5
Infinite System <81973574+InfiniteSystem20@users.noreply.github.com>
2021-05-10 11:00:59 -0300

    Criação do projeto Dominio
    Classes do Projeto

---
+++ Dominios/Cliente.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominios
+{
+    public class Cliente
+    {
+        public string login_id_cli { get; set; }
+        public string senha_cli { get; set; }
+        public string nome_cli { get; set; }
+        public string cpf_cli { get; set; }
+        public string telefone_cli { get; set; }
+        public string email_cli { get; set; }
+        public string endereco_cli { get; set; }
+        public string complemento_cli { get; set; }
+        public string bairro_cli { get; set; }
+        public string cidade_cli { get; set; }
+        public string uf_id_est { get; set; }
+        public string cep_cli { get; set; }
+
+    }
+}
---
+++ Dominios/Usuario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dominios
+{
+    public class Usuario
+    {
+        public int IdUsu { get; set; }
+        public string NomeUsu { get; set; }
+        public string Cargo { get; set; }
+        public string Senha { get; set; }
+        public DateTime DataNasc { get; set; }
+    }
+}
