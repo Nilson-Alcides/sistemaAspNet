a5d8ff54e700180baf47213b819523de3388a9f5
Infinite System <81973574+InfiniteSystem20@users.noreply.github.com>
2021-05-12 16:54:56 -0300

    Atualizaçã clienteDAO

--- ModeloDLL/ClienteDAO.cs
+++ ModeloDLL/ClienteDAO.cs
@@ -15,8 +15,9 @@ namespace ModeloDLL
 
         public void Insert(Cliente cliente)
         {
-            string strInsert = string.Format("insert into cliente(login_id_cli, senha_cli, nome_cli, cpf_cli, telefone_cli, endereco_cli, complemento_cli, email_cli, bairro_cli, cidade_cli, uf_id_est, cep_cli) " +
-                "values('{0}','{1}','{2}','{3}','{4}','{5}','{6}','{7}','{8}','{9}','{10}','{11}','{12}');",
+            string strInsert = string.Format("insert into cliente(login_id_cli, senha_cli, nome_cli, cpf_cli, telefone_cli, endereco_cli," +
+                " complemento_cli, email_cli, bairro_cli, cidade_cli, uf_id_est, cep_cli) " +
+                " values('{0}','{1}','{2}','{3}','{4}','{5}','{6}','{7}','{8}','{9}','{10}','{11}','{12}');",
                     cliente.login_id_cli, cliente.senha_cli, cliente.nome_cli, cliente.cpf_cli, cliente.telefone_cli, cliente.endereco_cli,
                     cliente.complemento_cli, cliente.email_cli, cliente.bairro_cli, cliente.cidade_cli, cliente.uf_id_est, cliente.cep_cli);
             db.ExecutaComando(strInsert);
