d936e6a92f83985301b486d40f321e134ad2d7b0
Nilson Alcides <nilson_alcides@hotmail.com.com>
2021-05-30 21:59:21 -0300

    Atualização do cliente e funcionário
    Atualização do cliente e funcionário ate a atualizar(UPDATE)

--- Dominios/Cliente.cs
+++ Dominios/Cliente.cs
@@ -15,8 +15,8 @@ namespace Dominios
 
         [Display(Name = "Nome ", Description = "Nome e Sobrenome.")]
         [Required(ErrorMessage = "O nome completo é obrigatório.")]
-        [RegularExpression(@"^[a-zA-Z''-'\s]{1,40}$", ErrorMessage =
-            "Números e caracteres especiais não são permitidos no nome.")]
+        //[RegularExpression(@"^[a-zA-Z''-'\s]{1,40}$", ErrorMessage =
+        //    "Números e caracteres especiais não são permitidos no nome.")]
         public string nomeCliente { get; set; }
 
         [Display(Name = "CPF")]
@@ -27,6 +27,8 @@ namespace Dominios
         [EmailAddress(ErrorMessage = " O Email não é valido")]
         [RegularExpression(".+\\@.+\\..+", ErrorMessage = "Informe um email válido...")]
         public string emailCliente { get; set; }
+
+        public Endereco endereco { get; set; }
 
         //[DataType(DataType.PhoneNumber)]
         //[RegularExpression(@"^\(\d{2}\)\d{4}-\d{4}$",
