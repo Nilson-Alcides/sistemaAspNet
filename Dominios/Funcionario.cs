c21aed6ebb05ab5aa6f01b8f0456b99bc880c3f4
Nilson Alcides <nilson_alcides@hotmail.com.com>
2021-06-01 23:59:01 -0300

    Atualização do Layout Accordion

--- Dominios/Funcionario.cs
+++ Dominios/Funcionario.cs
@@ -16,8 +16,8 @@ namespace Dominios
         // Nome do funcionario
         [Display(Name = "Nome", Description = "Nome e Sobrenome.")]
         [Required(ErrorMessage = "O nome completo é obrigatório.")]
-    ///    [RegularExpression(@"^[a-zA-Z''-'\s]{1,40}$", ErrorMessage =
-       //    "Números e caracteres especiais não são permitidos no nome.")]
+        // [RegularExpression(@"^[a-zA-Z''-'\s]{1,40}$", ErrorMessage =
+        //    "Números e caracteres especiais não são permitidos no nome.")]
         public string nomeFunc { get; set; }
         
         // CPF do funcionario
